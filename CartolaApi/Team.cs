b7deef84bad8b94aa1db9d63b2af46a706fcd0e1
joaovbressan <joaovictorteixeirabressan@gmail.com>
2024-09-19 16:38:32 -0300

    Delete CartolaApi/Team.cs

--- CartolaApi/Team.cs
+++
@@ -1,7 +0,0 @@
-public class Team
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public Tournament Tournament { get; set; } 
-    public List<Player> Players { get; set; } = new(); 
-}
