c84cdb82a28f7cbb0b94d9ddbede834228f51c44
joaovbressan <joaovictorteixeirabressan@gmail.com>
2024-09-19 17:05:13 -0300

    Update Team.cs

--- CartolaApi/Models/Team.cs
+++ CartolaApi/Models/Team.cs
@@ -2,6 +2,5 @@ public class Team
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public Tournament Tournament { get; set; } 
     public List<Player> Players { get; set; } = new(); 
 }
