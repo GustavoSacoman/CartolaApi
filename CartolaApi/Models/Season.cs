e908cdf61cb1681a4503f45acd2e54404223936e
Fernando Andrade <fernands75@hotmail.com>
2024-09-21 15:37:06 -0300

    Criando a lista Tournaments na model Season

--- CartolaApi/Models/Season.cs
+++ CartolaApi/Models/Season.cs
@@ -7,6 +7,7 @@ namespace CartolaApi.Models
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime FinalDate { get; set; }
+        public List<Tournament> Tournaments { get; set;} = new List<Tournament>();
     }
 }
 
