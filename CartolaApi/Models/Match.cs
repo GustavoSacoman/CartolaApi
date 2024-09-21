47db7ddfdbdab3dde1797d818e9077a54abb3bd6
Alquieri <guilherme.alquieri@gmail.com>
2024-09-21 00:22:59 -0300

    Matchs
    Atributos criados

---
+++ CartolaApi/Models/Match.cs
@@ -0,0 +1,18 @@
+
+namespace CartolaApi.Models
+{
+    public class Match
+    {
+        
+        public int IdMatch { get; set; }
+        public DateTime Date { get; set; }
+
+        public string? Result { get; set; }
+        
+        public int IdTeam1 { get; set; }
+        public int IdTeam2 { get; set; }
+
+        public int IdTournament { get; set; }
+
+    }
+}
