d65c14aa3068523b48890672a479439fc67c2aa8
Parcky <144289286+Parckyzin@users.noreply.github.com>
2024-09-21 22:01:53 -0300

    User db functions

--- CartolaApi/Models/User.cs
+++ CartolaApi/Models/User.cs
@@ -6,4 +6,5 @@ public class User
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
+    public required string Phone { get; set; }
 }
