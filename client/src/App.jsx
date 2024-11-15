02e39240d684ffc82a5ff242a5d86e776c39ac8c
Parcky <144289286+Parckyzin@users.noreply.github.com>
2024-11-15 01:25:57 -0300

    removed useless test components from now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login/Login';
-import Home from './pages/Home';
-import Profile from './pages/Profile/Profile';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
@@ -15,8 +13,6 @@ function App() {
             element={<Login />} /* TODO: Change to Home */
           />
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
     </AuthProvider>
